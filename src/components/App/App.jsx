fa315e413363eeb5b16b030d9e2f221cbc26ba74
Вікторія Юрченко <viktoriau26@gmail.com>
2021-09-01 21:46:37 +0300

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+// import './App.css';
+import { MainContainer } from "../MainContainer/MainContainer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MainContainer/>
+    </div>
+  );
+}
